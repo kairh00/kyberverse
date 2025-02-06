038e4a6c8bde6d2e64a910cc261824a94f2f398b
kairh00 <65600128+kairh00@users.noreply.github.com>
2025-02-06 21:22:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const theme = ["default", "fun","brutal",];
+//var button = document.getElementsByClassName("themer");
+let i =1;
+document.documentElement.className = theme[Math.floor((Math.random() * theme.length)+1)];
+
+function themechange(){
+document.documentElement.className = theme[i];
+  
+ if (i >= theme.length){ i=1;}
+  else {i++;}
+  console.log(i);
+  
+}                        
+  
+
+//button.addEventListener("click", themechange);
